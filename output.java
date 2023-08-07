7834ada5990606ffc2a8767b2c4f46de60bc93e5
DeveshxTrip <devesh2020it005@abesit.edu.in>
2023-08-07 21:42:56 +0530

    new addition

---
+++ output.java
@@ -0,0 +1,22 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = -50;
+		int b = 40;
+		// update your code below this line
+		int sum,product,quotient;
+		sum=a+b;
+		product=a*b;
+		quotient=a/b;
+		System.out.println(sum);
+		System.out.println(product);
+		System.out.println(quotient);
+
+		
+		
+		
+		
+		
+	}
+}
