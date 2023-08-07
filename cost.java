8ec245d21d269fe77046095da648db638247a3d0
DeveshxTrip <devesh2020it005@abesit.edu.in>
2023-08-07 21:38:52 +0530

    new problem

---
+++ cost.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int s = 14 ;
+        int area = 14*14;
+        int cost = area*7;
+        System.out.println(area);
+        System.out.print(cost + "$");
+	}
+}
